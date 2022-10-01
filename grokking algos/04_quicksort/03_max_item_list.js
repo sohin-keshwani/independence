35992f53416d820d84c1587b0685d8885ed1fc80
Sohin Keshwani <sohin@stylishop.com>
2022-10-01 17:47:55 +0400

    adding Number.MIN_SAFE_INTEGER instead of Number.MIN_VALUE to the base case and -ve array test case

--- grokking algos/04_quicksort/03_max_item_list.js
+++ grokking algos/04_quicksort/03_max_item_list.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const maxItemInList = function(arr)  {
-    if (arr.length == 0) return Number.MIN_VALUE;;
+    if (arr.length == 0) return Number.MIN_SAFE_INTEGER;
 
     return Math.max(arr[0] , maxItemInList(arr.slice(1)));
 };
 
-console.log(maxItemInList([1,7,3,44,5]))
+
+// console.log(Math.max(-1, Number.MIN_SAFE_INTEGER))
+// console.log(Math.max(-1, Number.MIN_SAFE_INTEGER));
+
+//console.log(maxItemInList([1,7,3,44,5]))
+
+console.log(maxItemInList([-1,-2,-3,-44,-5]))
