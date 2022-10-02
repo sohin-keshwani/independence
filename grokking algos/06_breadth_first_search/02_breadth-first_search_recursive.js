72dbf4a7a1c52103de14d8dad140d888309a5aa6
Sohin Keshwani <sohin@stylishop.com>
2022-10-02 17:27:07 +0400

    wip recurssion

--- grokking algos/06_breadth_first_search/02_breadth-first_search_recursive.js
+++ grokking algos/06_breadth_first_search/02_breadth-first_search_recursive.js
@@ -12,30 +12,27 @@ const personIsSeller = (name) => {
     return name[name.length - 1] === "m";
 }
 
-
+const searchRecursive = (q, searched) => {
+    if (q.length == 0) return false
+    let person = q.shift();
+    console.log(searched);
+    if (!(person in searched)) {
+        return searchRecursive(q.slice(1), searched);
+        if (personIsSeller(person)) {
+            console.log(`${person} is a mango seller`)
+            return true;
+        }
+        searched[person] = true;
+        return searchRecursive(q.concat(graph[person]), searched);
+    }
+}
 
 const search = (name) => {
     let q = []
     q = q.concat(graph[name]);
 
-    let searched = {}
-
-    while (q.length) {
-        let person = q.shift();
-        // console(person)
-        if (!person in searched) {
-            if (personIsSeller(person)) {
-                console.log(`${person} is a mango seller`)
-                return true;
-            } else {
-                q = q.concat(graph[person]);
-                searched[person] = true
-            }
-        } else {
-            console.log(`${person} was already looked up for mango seller`)
-        }
-    }
-    return false;
+    searchRecursive(q, {});
+    
 }
 
 search("you"); // thom is a mango seller!
