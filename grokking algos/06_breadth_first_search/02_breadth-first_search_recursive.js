cd03b5c2e57c14573e2e31607b1f2a84ed34323f
Sohin Keshwani <sohin@stylishop.com>
2022-10-04 06:13:36 +0400

    who is the mango seller, recursive approach

--- grokking algos/06_breadth_first_search/02_breadth-first_search_recursive.js
+++ grokking algos/06_breadth_first_search/02_breadth-first_search_recursive.js
@@ -11,20 +11,31 @@ graph["jonny"] = [];
 const personIsSeller = (name) => {
     return name[name.length - 1] === "m";
 }
+/*
+1. look at the first network and check if they are mango sellers
+1a. add alice, bob, claire to my queue
+1b. check if they are the mango sellers?
+1c. if not
+1d. if yes then terminate
+2. add alice, bob, and claire contacts to queue and repeat 1a to 1d
+*/
 
 const searchRecursive = (q, searched) => {
     if (q.length == 0) return false
-    let person = q.shift();
-    console.log(searched);
+    console.log(q)
+    console.log(JSON.stringify(searched))
+    let person = q.shift()
     if (!(person in searched)) {
-        return searchRecursive(q.slice(1), searched);
-        if (personIsSeller(person)) {
+        searched[person] = true
+        if (!personIsSeller(person)) {
+            // console.log(`${person} is not a mango seller`)
+            return searchRecursive(q.concat(graph[person]), searched)
+        } else {
             console.log(`${person} is a mango seller`)
             return true;
-        }
-        searched[person] = true;
-        return searchRecursive(q.concat(graph[person]), searched);
+        }   
     }
+    return searchRecursive(q, searched)
 }
 
 const search = (name) => {
